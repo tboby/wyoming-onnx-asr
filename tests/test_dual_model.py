2b9083f39d7ff33af05e074777c42409e377d9fd
Thomas Boby <th.boby@gmail.com>
2025-07-13 22:05:19 +0100

    Longer timeout

--- tests/test_dual_model.py
+++ tests/test_dual_model.py
@@ -21,7 +21,7 @@ _LOCAL_DIR = _PROGRAM_DIR / "local"
 _SAMPLES_PER_CHUNK = 1024
 
 # Need to give time for the model to download
-_START_TIMEOUT = 60
+_START_TIMEOUT = 90
 _TRANSCRIBE_TIMEOUT = 60
 
 
